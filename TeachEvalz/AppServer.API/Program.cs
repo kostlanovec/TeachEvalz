a7ab7b158717d46fd7be0227fdbc164e6defe57b
MartinRostejnsky <martin.rostejnsky@email.cz>
2023-08-09 19:40:58 +0200

    CORS adjustment & Grpc web support

--- TeachEvalz/AppServer.API/Program.cs
+++ TeachEvalz/AppServer.API/Program.cs
@@ -1,4 +1,6 @@
 using AppServer.API.Services;
+using Grpc.AspNetCore.Server;
+using Grpc.AspNetCore.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,12 +8,27 @@ var builder = WebApplication.CreateBuilder(args);
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
 
 // Add services to the container.
-builder.Services.AddGrpc();
+builder.Services.AddGrpc(options =>
+    {
+    options.IgnoreUnknownServices = false;
+    });
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin", builder => 
+    { 
+        builder.WithOrigins("https://localhost:7024")
+        .AllowAnyHeader()
+        .AllowAnyMethod(); 
+    });
+});
 
 var app = builder.Build();
 
+app.UseGrpcWeb();
+app.UseCors("AllowSpecificOrigin");
+
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+app.MapGrpcService<GreeterService>().EnableGrpcWeb();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
