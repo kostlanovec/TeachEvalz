87f37e912a5c51237dfecc95d452a0123cce5394
Jan S. Kostlán <jan.kostlan.021@pslib.cz>
2023-08-24 17:37:18 +0200

    neviim

--- AppServer.Core/Services/Interfaces/IGreetService.cs
+++ AppServer.Core/Services/Interfaces/IGreetService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AppServer.Core.Services.Interfaces
+﻿namespace AppServer.Core.Services.Interfaces
 {
     public interface IGreetService
     {
---
+++ AppServer.DataAccess/DataContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AppServer.DataAccess
+{
+	public class DataContext : DbContext
+	{
+		public DataContext(DbContextOptions<DataContext> options) : base(options)
+		{
+		}
+
+		/// <summary>
+		/// Definuje mapování mezi třídami a tabulkami databáze.
+		/// </summary>
+		/// <param name="modelBuilder">Třída, která je zodpovědná za sestavneí modelu</param>
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			
+			//Seed.Initialize(modelBuilder);
+
+			base.OnModelCreating(modelBuilder);
+		}
+		//#region DbSet
+		//// Definice DbSetů pro každou třídu, kterou chceme mapovat na databázovou tabulku.§
+		//public DbSet<User> Users { get; set; }
+		//public DbSet<NetPromoterScore> NetPromoterScores { get; set; }
+		//public DbSet<Server> Servers { get; set; }
+		//public DbSet<Merchant> Merchants { get; set; }
+		//#endregion
+
+	}
+}
